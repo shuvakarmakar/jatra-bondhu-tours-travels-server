3fa8320a8b4417d651cf8a8def8716fa153d643d
shuvakarmakar <shuva15-7953@diu.edu.bd>
2024-12-09 17:39:35 +0600

    Update index.js and installed dotenv for securities

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 
 app.use(cors());
 app.use = (express.json())
 
-// UeNs6jYWDbpWg7R0
-// jatraBondhu
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -18,7 +18,7 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
-const uri = "mongodb+srv://jatraBondhu:UeNs6jYWDbpWg7R0@cluster0.gu0z5kw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gu0z5kw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
