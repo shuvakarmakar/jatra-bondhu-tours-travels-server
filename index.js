1bfe812af3d2925bdeb865bd03f1e9b5931fda40
shuvakarmakar <shuva15-7953@diu.edu.bd>
2024-12-26 22:39:41 +0600

    Update .env File

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
+const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 
@@ -9,14 +10,24 @@ app.use(cors());
 app.use = (express.json())
 
 
+// token verification
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.send({ message: "No Token" })
+  }
+  const token = authorization.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_KEY_TOKEN, (err, decoded) => {
+    if (err) {
+      return res.send({ message: "Invalid Token" })
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gu0z5kw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -29,14 +40,40 @@ const client = new MongoClient(uri, {
 });
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+
+
+
+
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
+
+
+
+
+app.get('/', (req, res) => {
+  res.send('This is Jatra Bondhu Travel Server')
+})
+
+// JWT Authentication
+app.post('/authentication', (req, res) => {
+  const userEmail = req.body;
+
+  if (!userEmail || !userEmail.email) {
+    return res.status(400).send({ error: "Email is required" });
+  }
+
+  const token = jwt.sign(userEmail, process.env.ACCESS_KEY_TOKEN, { expiresIn: '10d' });
+  res.send({ token });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
